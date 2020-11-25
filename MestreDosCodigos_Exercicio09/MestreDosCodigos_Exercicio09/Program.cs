89c887e464f3b3938b6b6e25e0a33dc4e2f5040b
José Renan Faria <joserenan@outlook.com>
2020-11-24 23:16:29 -0300

    Exercicio09 incompleto
    Exercicio09 incompleto

--- MestreDosCodigos_Exercicio09/MestreDosCodigos_Exercicio09/Program.cs
+++ MestreDosCodigos_Exercicio09/MestreDosCodigos_Exercicio09/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MestreDosCodigos_Exercicio09
 {
@@ -6,7 +8,29 @@ namespace MestreDosCodigos_Exercicio09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("** Manuseando Listas com LINQ **");
+
+            var listaNumerosInteiros = new List<int>() { 1, 7, 19, 35, 2, 3 };
+
+            Console.WriteLine("\nImprimindo itens da lista \n");
+            listaNumerosInteiros.ForEach(Console.WriteLine);
+
+            Console.WriteLine("\nImprimindo itens da lista na ordem crescente \n");
+            var listaNumerosInteirosAscendente = listaNumerosInteiros.OrderBy(x => x).ToList();
+            listaNumerosInteirosAscendente.ForEach(Console.WriteLine);
+
+            Console.WriteLine("\nImprimindo itens da lista na ordem decrescente \n");
+            var listaNumerosInteirosDecrescente = listaNumerosInteiros.OrderByDescending(x => x).ToList();
+            listaNumerosInteirosDecrescente.ForEach(Console.WriteLine);
+
+            Console.WriteLine("\nImprimindo primeiro item da lista");
+            Console.WriteLine(listaNumerosInteiros.First());
+
+            Console.WriteLine("\nImprimindo último item da lista");
+            Console.WriteLine(listaNumerosInteiros.Last());
+
+            Console.ReadKey();
+
         }
     }
 }
