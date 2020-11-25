a5760d32cf1f605631cc1fad8c10e31e8b228817
José Renan Faria <joserenan@outlook.com>
2020-11-24 22:03:10 -0300

    Exercicio07
    Exercicio07

--- MestreDosCodigos_Exercicio07/MestreDosCodigos_Exercicio07/Program.cs
+++ MestreDosCodigos_Exercicio07/MestreDosCodigos_Exercicio07/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MestreDosCodigos_Exercicio07
 {
@@ -7,6 +9,32 @@ namespace MestreDosCodigos_Exercicio07
         static void Main(string[] args)
         {
             Console.WriteLine("** Ler números inteiros e calcular os pares **");
+
+            var listaNumerosInteiros = new List<int>();
+
+            Console.Write("\nDigite o primeiro número inteiro: ");
+            int primeiroNumero = int.Parse(Console.ReadLine());
+            listaNumerosInteiros.Add(primeiroNumero);
+
+            Console.Write("\nDigite o segundo número inteiro: ");
+            int segundoNumero = int.Parse(Console.ReadLine());
+            listaNumerosInteiros.Add(segundoNumero);
+
+            Console.Write("\nDigite o terceiro número inteiro: ");
+            int terceiroNumero = int.Parse(Console.ReadLine());
+            listaNumerosInteiros.Add(terceiroNumero);
+
+            Console.Write("\nDigite o quarto número inteiro: ");
+            int quartoNumero = int.Parse(Console.ReadLine());
+            listaNumerosInteiros.Add(quartoNumero);
+
+            listaNumerosInteiros.RemoveAll(x => x % 2 != 0);
+
+            int somaNumerosPares = listaNumerosInteiros.Sum();
+
+            Console.WriteLine($"\nA soma dos números inteiros pares é: {somaNumerosPares}");
+
+            
         }
     }
 }
